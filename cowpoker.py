4c7577d15d3c42e6e03f9d6b2276fcc3907e95a3
Paul Anderson <ander1x@uwindsor.ca>
2013-08-17 09:48:53 -0500

    Added command line colour support.

---
+++ cowpoker.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#CowPoker - released under current GPL by PVA.
+# Game wherein one assaults a cow.
+
+from colorama import init
+init()
+
+from colorama import Fore, Back, Style
+print(Fore.RED + 'gribblesnarker')
+
