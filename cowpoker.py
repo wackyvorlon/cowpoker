21bc225785728d893442902590070eba5057b378
Paul Anderson <ander1x@uwindsor.ca>
2013-08-17 11:56:41 -0500

    Now handles aggravation of the cow.

--- cowpoker.py
+++ cowpoker.py
@@ -17,6 +17,7 @@ x=1
 
 moos=0
 coins=0
+aggravation=0
 
 f=open("nouns.txt",'r')
 
@@ -28,6 +29,7 @@ def cowpoke():
 	global moos
 	global coins 
 	global words
+	global aggravation
 	poked = random.randrange(0,10)
 	#print poked
 	if poked<=4:
@@ -36,6 +38,13 @@ def cowpoke():
 	else:
 	    print('\nThe cow glances over at you,\nthen returns to chewing his cud and contemplating {noun}.').format(noun=random.choice(words).rstrip())
 	    print('\nHe briefly wonders if you have nothing better to do.')
+	    aggravation=aggravation+1
+	    if aggravation>10:
+	    	print("\nNow you've done it!")
+	    	print(Fore.RED+'The cow looks at you, then charges, flattening you on top\nof a large pile of manure.'+Fore.WHITE)
+	    	if coins>0:
+	    		print('You dropped your coins, now the cow is standing on them!')
+	    		coins=0
 	if moos>5:
 		print('\nThe cow gives an unamused stare,\nand promptly coughs up a coin.')
 		print(Fore.BLUE+"\nIt's a golden Roman aureus of the Julio-Claudians,\nminted in 8 BC!"+Fore.WHITE)
@@ -45,16 +54,25 @@ def cowpoke():
 		moos=0
 		coins=coins+1
 
+def feed():
+	#Handles feeding the cow.
+	global aggravation
+	if aggravation>0:
+		aggravation=aggravation-1
+		print(Fore.CYAN+"\nHe's starting to calm down now.")
 
 while x:
 	if coins!=0:
 		print(Style.DIM+'\nYou have '+str(coins)+' coins!'+Style.RESET_ALL)
+	if aggravation!=0:
+		print(Style.DIM+Fore.RED+'\nThe cow has an aggravation level of '+str(aggravation)+'! Be careful!'+Style.RESET_ALL)
 	cmd = raw_input(Fore.GREEN +'Do you want to poke or feed the cow? (p/f) '+Fore.WHITE)
 	if cmd =='p':
 		print('Poked!')
 		cowpoke()
 	elif cmd == 'f':
 		print('You give the cow a hand-full of hay.\nHe grunts approvingly then drools on your hand.')
+		feed()
 	elif cmd == 'x':
 		x=0
 
