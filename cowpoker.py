4cd188b5e20a3fc5e6436d31c0a061331c97784b
Paul Anderson <ander1x@uwindsor.ca>
2013-08-17 10:34:49 -0500

    Most of the logic is there.

--- cowpoker.py
+++ cowpoker.py
@@ -15,16 +15,29 @@ random.seed()
 print(Fore.WHITE+'\n\nYou are standing in an open field.\nBefore you stands a generic cow.\n')
 x=1 
 
+moos=0
+coins=0
+
 def cowpoke():
 	#Step 1: Did we succeed in poking the cow?
-	#random.seed() 
+	#random.seed()
+	global moos
+	global coins 
 	poked = random.randrange(0,10)
 	print poked
 	if poked<=4:
 		print('The cow mooed!')
+		moos=moos+1
 	else:
 	    print('The cow glances over at you, then returns to chewing his cud.')
 	    print('He briefly wonders if you have nothing better to do.')
+	if moos>5:
+		print('The cow gives an unamused stare,\nand promptly coughs up a coin.')
+		print(Fore.BLUE+"It's a golden Roman aureus of the Julio-Claudians,\nminted in 8 BC!"+Fore.WHITE)
+		if coins==0:
+			print('Someone has been using this cow to smuggle antiquities!')
+		print('I wonder if he has more?')
+		coins=coins+1
 
 
 while x:
