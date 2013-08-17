22525e5ce421ad03bb5cc5dd73fe8ca2ca5350b9
Paul Anderson <ander1x@uwindsor.ca>
2013-08-17 10:54:40 -0500

    Coming along.

--- cowpoker.py
+++ cowpoker.py
@@ -20,18 +20,21 @@ coins=0
 
 f=open("nouns.txt",'r')
 
+words=f.readlines()
+
 def cowpoke():
 	#Step 1: Did we succeed in poking the cow?
 	#random.seed()
 	global moos
 	global coins 
+	global words
 	poked = random.randrange(0,10)
 	#print poked
 	if poked<=4:
 		print(Fore.RED+'The cow mooed!'+Fore.WHITE)
 		moos=moos+1
 	else:
-	    print('The cow glances over at you, then returns to chewing his cud.')
+	    print('The cow glances over at you,\nthen returns to chewing his cud and contemplating {noun}.').format(noun=random.choice(words))
 	    print('He briefly wonders if you have nothing better to do.')
 	if moos>5:
 		print('\nThe cow gives an unamused stare,\nand promptly coughs up a coin.')
